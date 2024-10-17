ae0d8d8e5764c40ed2817866ff9b7592550d4157
abinwilson500 <abinwilson500@gmail.com>
2024-10-17 12:30:31 +0530

    note app

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/adapters.dart';
 import 'package:noteapp/note_page.dart';
 
-void main() {
+void main() async{
+  await Hive.initFlutter();
+  var box=await Hive.openBox('mybox');
   runApp(const MyApp());
 }
 
--- lib/note_page.dart
+++ lib/note_page.dart
@@ -1,31 +1,59 @@
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
 
 class NotePage extends StatefulWidget{
   @override
   State<NotePage> createState()=> _NotePageState();
 }
 class _NotePageState extends State<NotePage>{
+  late Box box;
+
+  List<Map<String,String>> itemList=[];
+
+  TextEditingController titleController=TextEditingController();
+   TextEditingController descriptionController=TextEditingController();
+    TextEditingController dateController=TextEditingController();
+     TextEditingController categoryController=TextEditingController();
+      
+  @override
+  void initState(){
+    super.initState();
+    box=Hive.box('mybox');
+
+    final storedItems= box.get('itemList');
+    if(storedItems is List){
+      itemList=List<Map<String,String>>.from(
+        storedItems.map((e) => Map<String ,String>.from(e))
+      );
+    }
+  }
   @override
   Widget build(BuildContext context){
     return Scaffold(
 
       appBar: AppBar(title: Text("Note App Example"),),
 
-      body: GridView.builder(gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2), itemBuilder: (context,index){
-        return Container(
-          height: 100,
-          width: double.infinity,
-         color: const Color.fromARGB(255, 252, 166, 195),
-         child: Column(
-          children: [
-            Text(""),
-            Text(""),
-            Text(""),
-            Text(""),
-          ],
-         ),
-
-
+      body: GridView.builder(gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2,crossAxisSpacing: 10,mainAxisSpacing: 10), 
+      itemCount: itemList.length,
+      itemBuilder: (context,index){
+        final item=itemList[index];
+        return Padding(
+          padding: const EdgeInsets.all(20),
+          child: Container(
+            height: 50,
+            width: 50,
+           decoration: BoxDecoration(borderRadius: BorderRadius.circular(15),color: Colors.pink),
+           child: Column(
+            children:[
+              Text(item['title'] ?? 'No Title'),
+              Text(item['description'] ?? 'No Description'),
+              Text(item['category'] ?? 'No Category'),
+              Text(item['date'] ?? 'No Date'),
+            ],
+           ),
+          
+          
+          ),
         );
       }),
      
@@ -36,27 +64,57 @@ class _NotePageState extends State<NotePage>{
           width: double.infinity,
           child: Padding(
             padding: const EdgeInsets.all(20),
+          
             child: Column(
               
               children: [
                  SizedBox(height: 30,),
-                TextField(decoration: InputDecoration(border: OutlineInputBorder(),hintText: 'title'),),
+                TextField(
+                  controller: titleController,
+                  decoration: InputDecoration(border: OutlineInputBorder(),hintText: 'title'),),
                 SizedBox(height: 20,),
-               TextField(decoration: InputDecoration(border: OutlineInputBorder(),hintText: 'description'),),
+               TextField(
+                controller: descriptionController,
+                decoration: InputDecoration(border: OutlineInputBorder(),hintText: 'description'),),
                  SizedBox(height: 20,),
                 
-               TextField(decoration: InputDecoration(border: OutlineInputBorder(),hintText: 'category'),),
+               TextField(
+                controller: categoryController,
+                decoration: InputDecoration(border: OutlineInputBorder(),hintText: 'category'),),
                  SizedBox(height: 20,),
-                TextField(decoration: InputDecoration(border: OutlineInputBorder(),hintText: 'date'),),
+                TextField(
+                  controller: dateController,
+                  decoration: InputDecoration(border: OutlineInputBorder(),hintText: 'date'),),
                 SizedBox(height: 100,),
                 
                 Row(
                   mainAxisAlignment: MainAxisAlignment.center,
                   crossAxisAlignment: CrossAxisAlignment.center,
                   children: [
-                    ElevatedButton(onPressed: (){}, child: Text("Add")),
+                    ElevatedButton(onPressed: (){
+                      setState(() {
+                        itemList.add({
+                          'title':titleController.text,
+                          'description':descriptionController.text,
+                          'caregory':categoryController.text,
+                          'date':dateController.text,
+                        });
+
+                        box.put('itemList', itemList.map((e)=> Map<String,dynamic>.from(e)).toList());
+                      });
+
+                      titleController.clear();
+                      descriptionController.clear();
+                      categoryController.clear();
+                      dateController.clear();
+
+                      Navigator.pop(context);
+
+                    }, child: Text("Add")),
                     SizedBox(width:30),
-                    ElevatedButton(onPressed: (){}, child: Text("Cancel")),
+                    ElevatedButton(onPressed: (){
+                      Navigator.pop(context);
+                    }, child: Text("Cancel")),
                   ],
                 ),
               ],
